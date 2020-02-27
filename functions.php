882ac3b35d3861df1fa62eff32eb06d4cbf63a6a
Baheux <fbaheux@gmail.com>
2020-02-27 10:44:04 +0100

    Theme v0.1

---
+++ functions.php
@@ -0,0 +1,100 @@
+<?php
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+// BEGIN ENQUEUE PARENT ACTION
+// AUTO GENERATED - Do not modify or remove comment markers above or below:
+
+if ( !function_exists( 'chld_thm_cfg_locale_css' ) ):
+    function chld_thm_cfg_locale_css( $uri ){
+        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) )
+            $uri = get_template_directory_uri() . '/rtl.css';
+        return $uri;
+    }
+endif;
+add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
+
+if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
+    function chld_thm_cfg_parent_css() {
+        wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array(  ) );
+    }
+endif;
+add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
+
+// END ENQUEUE PARENT ACTION
+
+
+
+
+function pr_scripts_styles() {
+    wp_enqueue_script('jquery'); // just enqueue as its already registered
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) )
+        wp_enqueue_script( 'comment-reply' );
+    /*   REGISTER ALL JS FOR SITE */;
+    wp_register_script('mainJS', get_stylesheet_directory_uri() . '/main.js');
+    /*   CALL ALL CSS AND SCRIPTS FOR SITE */
+    wp_enqueue_script('mainJS');
+ }
+ add_action( 'wp_enqueue_scripts', 'pr_scripts_styles' );
+
+
+
+
+
+
+ add_filter( 'woocommerce_subcategory_count_html', 'wc_filter_woocommerce_subcat_count_html', 10, 2 );
+
+ function wc_filter_woocommerce_subcat_count_html( $mark_class_count_category_count_mark, $category ) {
+     $pluriel ="";
+     if ($category->count > 1){
+        $pluriel ="s";
+     }
+    $mark_class_count_category_count_mark = ' <mark class="count">' . $category->count . ' Produit'. $pluriel .'</mark>';
+    return $mark_class_count_category_count_mark;
+ };
+
+
+
+
+
+ function rs_woo_cart_attributes($cart_item, $cart_item_key) {
+    global $product; 
+    if (is_cart()){ 
+        echo "<style>#checkout_thumbnail{visibility:hidden;}</style>"; 
+    } 
+    $item_data = $cart_item_key['data']; 
+    $post = get_post($item_data->get_id()); 
+    $thumb = get_the_post_thumbnail($item_data->get_id(), array( 32, 50)); 
+    echo '<div id="checkout_thumbnail" style="float: left; padding-right: 8px">' . $thumb . '</div> ' . $post->post_title;
+}
+
+add_filter('woocommerce_cart_item_name', 'rs_woo_cart_attributes', 10, 2);
+
+
+
+
+
+
+
+/**
+ * @snippet       Print List of Category IDs @ Product Categories Admin
+ * @author        Rodolfo Melogli
+ * @compatible    WooCommerce 3.7
+ * @donate $9     https://businessbloomer.com/bloomer-armada/
+ */
+  
+add_action( 'product_cat_pre_add_form', 'bbloomer_list_all_product_cat_ids', 5 );
+  
+function bbloomer_list_all_product_cat_ids() {
+  
+$ids = '';
+  
+$categories = get_categories( array(               
+   'taxonomy' => 'product_cat' ) );
+   
+foreach( $categories as $category ) {
+    $ids .= $category->term_id . ', ';
+} 
+  
+echo 'Category IDs: ' . $ids;
+}
---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+jQuery( document ).ready(function($) {
+
+    //supprime le terme "article"
+    if($('.et-cart-info').length > 0){
+        console.log($('.et-cart-info'));
+    }    
+
+
+});
