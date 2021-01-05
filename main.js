5d89c53567478516be5b386d6deafaf96e53b77d
Baheux <fbaheux@gmail.com>
2021-01-05 18:06:22 +0100

    ACF - affichage lien telechargement fiches

--- main.js
+++ main.js
@@ -8,4 +8,22 @@ jQuery( document ).ready(function($) {
     }    
 
 
+    //Gestionnaire de document
+    //FDS
+    if ($('#pdf_FDS').length > 0)
+    {
+        if ($('#pdf_FDS .et_pb_module_header a').length > 0)
+        {
+            $('#pdf_FDS').css('display', 'block');
+        }
+    }
+    //Fiche Technique
+    if ($('#pdf_FT').length > 0)
+    {
+        if ($('#pdf_FT .et_pb_module_header a').length > 0)
+        {
+            $('#pdf_FT').css('display', 'block');
+        }
+    }
+
 });
